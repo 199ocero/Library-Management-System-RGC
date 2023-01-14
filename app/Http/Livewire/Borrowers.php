718ab18694592a7df9e84f71518cab49da143923
freelanceocero <199ocero@gmail.com>
2023-01-14 10:18:05 +0800

    ⚡️ add store functionality

--- app/Http/Livewire/Borrowers.php
+++ app/Http/Livewire/Borrowers.php
@@ -12,6 +12,9 @@ class Borrowers extends Component
 
     protected $paginationTheme = 'bootstrap';
 
+    // declare variable from borrower livewire component
+    public $student_id, $full_name, $address, $contact_number;
+
     // declare variable for search filter
     public $search = '';
 
@@ -19,6 +22,51 @@ class Borrowers extends Component
     public function updatingSearch()
     {
         $this->resetPage();
+    }
+
+    // create a rule to validate the input fields
+    protected $rules = [
+        'student_id' => 'required|string|size:10|digits:10',
+        'full_name' => 'required|string',
+        'address' => 'required|string',
+        'contact_number' => 'required|string|size:11|digits:11',
+    ];
+
+    // function to store borrower
+    public function store()
+    {
+        $this->validate();
+
+        // save borrower if validation is success
+        Borrower::create([
+            'student_id' => $this->student_id,
+            'full_name' => $this->full_name,
+            'address' => $this->address,
+            'contact_number' => $this->contact_number,
+        ]);
+
+        // call this to reset modal fields and validation
+        $this->resetFieldsAndValidation();
+
+        // dispatch event to close the modal
+        $this->dispatchBrowserEvent('close-modal');
+
+        // dispatch event to show sweet alert 2
+        $this->dispatchBrowserEvent('swal', [
+            'title' => 'Borrower created successfully!',
+            'icon' => 'success',
+            'iconColor' => 'green',
+        ]);
+    }
+
+    // function for reseting the fields
+    public function resetFieldsAndValidation()
+    {
+        // call this to reset modal fields
+        $this->reset(['student_id', 'full_name', 'address', 'contact_number']);
+
+        // call this to reset validation error message
+        $this->resetValidation();
     }
 
     public function render()
