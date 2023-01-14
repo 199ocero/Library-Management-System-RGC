8ea66bc1dc6c4d43a82f2362af799b55e5316c15
freelanceocero <199ocero@gmail.com>
2023-01-14 08:00:27 +0800

    ⚡️ add book borrower

---
+++ database/migrations/2023_01_13_235657_create_borrowers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrowers', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id')->unique();
+            $table->string('full_name');
+            $table->string('address');
+            $table->string('contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrowers');
+    }
+}
