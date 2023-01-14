5f287adfda9745c75f8e17b1bf45d4cb93ce8281
freelanceocero <199ocero@gmail.com>
2023-01-14 21:36:48 +0800

    ⚡️ add store functionality

--- app/Http/Livewire/Inventories.php
+++ app/Http/Livewire/Inventories.php
@@ -15,7 +15,7 @@ class Inventories extends Component
 
     protected $paginationTheme = 'bootstrap';
 
-    public $books, $borrowers, $book_name, $borrower_name, $date_borrowed, $date_returned;
+    public $books, $borrowers, $book_name, $borrower_name, $date_borrowed, $amount;
 
     // listener for destroy an resetFieldsAndValidation
     protected $listeners = ['destroy', 'resetFieldsAndValidation'];
@@ -26,11 +26,46 @@ class Inventories extends Component
         $this->borrowers = Borrower::latest()->get();
     }
 
+    // create a rule to validate the input fields
+    protected $rules = [
+        'book_name' => 'required',
+        'borrower_name' => 'required',
+        'date_borrowed' => 'required',
+        'amount' => 'required|integer',
+    ];
+
+    // function to store borrower
+    public function store()
+    {
+        $this->validate();
+
+        // save borrower if validation is success
+        Inventory::create([
+            'book_id' => $this->book_name,
+            'borrower_id' => $this->borrower_name,
+            'date_borrowed' => $this->date_borrowed,
+            'amount' => $this->amount,
+        ]);
+
+        // call this to reset modal fields and validation
+        $this->resetFieldsAndValidation();
+
+        // dispatch event to close the modal
+        $this->dispatchBrowserEvent('close-modal');
+
+        // dispatch event to show sweet alert 2
+        $this->dispatchBrowserEvent('swal', [
+            'title' => 'Inventory created successfully!',
+            'icon' => 'success',
+            'iconColor' => 'green',
+        ]);
+    }
+
     // function for reseting the fields
     public function resetFieldsAndValidation()
     {
         // call this to reset modal fields
-        $this->reset(['book_name', 'date_borrowed', 'date_returned']);
+        $this->reset(['book_name', 'borrower_name', 'date_borrowed', 'amount']);
 
         // call this to reset validation error message
         $this->resetValidation();
--- resources/views/livewire/inventories/inventories-create.blade.php
+++ resources/views/livewire/inventories/inventories-create.blade.php
@@ -12,34 +12,46 @@
                 </div>
                 <form wire:submit.prevent="store">
                     <div class="modal-body">
-                        <div class="mb-3" wire:ignore>
-                            <label for="student_id" class="form-label">Select Borrower Name</label>
-                            <select wire:model='borrower_name' class="form-control selectpicker" data-live-search="true"
-                                data-size="5" title="Select borrower...">
-                                @foreach ($borrowers as $borrower)
-                                    <option value="{{ $borrower->id }}">{{ $borrower->full_name }}</option>
-                                @endforeach
-                            </select>
-                            @error('book_name')
+                        <div class="mb-3">
+                            <div wire:ignore>
+                                <label for="borrower_name" class="form-label">Select Borrower Name</label>
+                                <select wire:model='borrower_name' class="form-control selectpicker"
+                                    data-live-search="true" data-size="5" title="Select borrower...">
+                                    @foreach ($borrowers as $borrower)
+                                        <option value="{{ $borrower->id }}">{{ $borrower->full_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            @error('borrower_name')
                                 <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
-                        <div class="mb-3" wire:ignore>
-                            <label for="student_id" class="form-label">Select Book Name</label>
-                            <select wire:model='book_name' class="form-control selectpicker" data-live-search="true"
-                                data-size="5" title="Select book...">
-                                @foreach ($books as $book)
-                                    <option value="{{ $book->id }}">{{ $book->book_name }}</option>
-                                @endforeach
-                            </select>
+                        <div class="mb-3">
+                            <div wire:ignore>
+                                <label for="book_name" class="form-label">Select Book Name</label>
+                                <select wire:model='book_name' class="form-control selectpicker" data-live-search="true"
+                                    data-size="5" title="Select book...">
+                                    @foreach ($books as $book)
+                                        <option value="{{ $book->id }}">{{ $book->book_name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
                             @error('book_name')
                                 <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
                         <div class="mb-3">
-                            <label for="student_id" class="form-label">Select Date Borrowed</label>
+                            <label for="date_borrowed" class="form-label">Select Date Borrowed</label>
                             <input type="date" class="form-control" id="date_borrowed" wire:model="date_borrowed">
-                            @error('book_name')
+                            @error('date_borrowed')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label for="amount" class="form-label">Books to be Borrowed</label>
+                            <input type="number" class="form-control" id="date_borrowed" wire:model="amount"
+                                placeholder="Enter amount">
+                            @error('amount')
                                 <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
