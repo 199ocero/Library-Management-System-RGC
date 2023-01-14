fdfbe600b2bb93c08a098beb154d102ef593d3c6
freelanceocero <199ocero@gmail.com>
2023-01-14 11:51:11 +0800

    ⚡️ add book inventory

---
+++ app/Http/Livewire/Inventories.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Inventories extends Component
+{
+    public function render()
+    {
+        return view('livewire.inventories.inventories');
+    }
+}
---
+++ database/migrations/2023_01_14_033835_create_inventories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')->constrained();
+            $table->foreignId('borrower_id')->constrained();
+            $table->integer('amount');
+            $table->string('date_borrowed');
+            $table->string('date_returned');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
